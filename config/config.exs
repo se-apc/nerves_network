9618ebf3eb5f5ce9309fbc59ec6acff5d895834a
Tomasz Kazimierz Motyl <tomasz.motyl@schneider-electric.com>
2017-11-29 10:24:56 -0800

    Correcting a syntax error

--- config/config.exs
+++ config/config.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 key_mgmt = System.get_env("NERVES_NETWORK_KEY_MGMT") || "WPA-PSK"
 
-config :nerves_network, :default
+config :nerves_network, :default,
   wlan0: [
     ssid: System.get_env("NERVES_NETWORK_SSID"),
     psk: System.get_env("NERVES_NETWORK_PSK"),
