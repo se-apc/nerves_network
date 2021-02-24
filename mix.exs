d703fdc19e396b1e8baf6b54a2c24aed387dd10f
Tomasz Kazimierz Motyl <tomasz.motyl@se.com>
2021-02-24 05:17:43 -0800

    Because the nerves_network is no-longer maintained we switch to our nerves_network_interface fork indefinitely

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule Nerves.Network.Mixfile do
   defp deps do
     [
       {:system_registry, "~> 0.4"},
-      {:nerves_network_interface, "~> 0.4.0"},
+      {:nerves_network_interface, github: "se-apc/nerves_network_interface", branch: "master", override: true},
       {:nerves_wpa_supplicant, "~> 0.3.0"},
       {:elixir_make, "~> 0.4", runtime: false},
       {:ex_doc, "~> 0.11", only: :dev},
